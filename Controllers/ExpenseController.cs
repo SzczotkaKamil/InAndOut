92a0509d7faac2d3d7263186e1c6cea1c271545c
SzczotkaKamil <szkamil1999@wp.pl>
2021-07-20 08:50:44 +0200

    Created Update Action for Expenses

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -78,6 +78,36 @@ namespace InAndOut.Controllers
 
         }
 
+        public IActionResult Update(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Expense obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+        }
+
 
 
     }
